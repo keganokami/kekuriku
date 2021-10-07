bf6aa9b933ae8ca4c65c3b00751b9774e61ce340
keganokami <400800m@gmail.com>
2021-10-07 14:27:23 +0000

    login impl and nuxt,ts useing setting config

--- src/rakuriku/pages/login.vue
+++ src/rakuriku/pages/login.vue
@@ -1,15 +1,10 @@
 <template>
-  <v-row
-    class="login-card flex-column"
-    height="100%"
-    justify="center"
-    align-content="center"
-  >
-    <v-card height="400" max-width="670">
+  <div class="login-card flex-column d-flex justify-center align-center pa-6">
+    <v-card height="400" width="670">
       <v-card-title class="justify-center mt-7">
         <h2>ログイン</h2>
       </v-card-title>
-      <v-card-text class="d-flex flex-column justify-center align-center mt-7">
+      <div class="flex-column d-flex justify-center align-center mt-7">
         <div class="signin-form mb-4">
           <v-text-field label="RakurikuID" outlined v-model="id"></v-text-field>
         </div>
@@ -25,55 +20,53 @@
         </div>
         <v-card-actions>
           <NuxtLink to="#">
-            <v-btn dark width="96" color="#4169e1">
-              ログイン
-            </v-btn>
+            <v-btn dark width="96" color="#4169e1" @click="login()"> ログイン </v-btn>
           </NuxtLink>
         </v-card-actions>
-      </v-card-text>
+      </div>
     </v-card>
-    <v-card height="254" width="670" class="mt-10">
+    <v-card
+      height="254"
+      width="670"
+      class="mt-10 flex-column d-flex justify-center align-center"
+    >
       <v-card-title class="flex-column mt-7">
         <h2>新規登録</h2>
         <p class="mt-8"><span>※</span>ログインには会員登録が必要です。</p>
       </v-card-title>
-      <v-card-text class="d-flex justify-center">
-        <v-card-actions>
-          <NuxtLink to="/signup">
-            <v-btn dark width="170" color="#4169e1">
-              新規登録
-            </v-btn>
-          </NuxtLink>
-        </v-card-actions>
-      </v-card-text>
+      <v-card-actions>
+        <NuxtLink to="/signup">
+          <v-btn dark width="170" color="#4169e1"> 新規登録 </v-btn>
+        </NuxtLink>
+      </v-card-actions>
     </v-card>
-  </v-row>
+  </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      id: "",
-      password: "",
-      isHiddenPassword: false
-    };
-  },
-  methods: {
-    async login() {
-      await this.$store.dispatch("login", {
-        userId: this.id,
-        password: this.password
-      });
-    }
+<script lang="ts">
+import { Component, Vue} from 'nuxt-property-decorator';
+
+@Component
+export default class Login extends Vue {
+  id = "";
+  password = "";
+  isHiddenPassword = false;
+  $store: any;
+
+  async login() {
+    await this.$store.dispatch("login", {
+      userId: this.id,
+      password: this.password,
+    });
   }
-};
+}
 </script>
 
 <style scoped>
 .login-card {
   /* 背景色は決まり次第変更 */
   background-color: rgba(66, 175, 237, 0.96);
+  height: 100%;
 }
 
 .signin-form {
