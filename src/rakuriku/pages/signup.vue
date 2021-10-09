fc6c80ab6d2027076e4739d23e84afe5d1c4ce78
shogo4131 <shougo80697118@gmail.com>
2021-10-09 18:53:50 +0900

    feat: create signup form

--- src/rakuriku/pages/signup.vue
+++ src/rakuriku/pages/signup.vue
@@ -1,5 +1,79 @@
 <template>
-  <div>
-    ユーザー登録画面
+  <div class="login-card flex-column d-flex justify-center align-center pa-6">
+    <v-card height="628" width="670">
+      <v-card-title class="justify-center mt-7">
+        <h2>新規登録</h2>
+      </v-card-title>
+      <div class="flex-column d-flex justify-center align-center mt-7">
+        <div class="signup-form mb-5">
+          <v-text-field label="らくりくID" outlined v-model="id"></v-text-field>
+        </div>
+        <div class="signup-form mb-5">
+          <v-text-field
+            type="email"
+            label="メールアドレス"
+            outlined
+            v-model="email"
+          ></v-text-field>
+        </div>
+        <div class="signup-form mb-5">
+          <v-text-field
+            label="パスワード"
+            outlined
+            :append-icon="isHiddenPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="isHiddenPassword ? 'text' : 'password'"
+            @click:append="isHiddenPassword = !isHiddenPassword"
+            v-model="password"
+          ></v-text-field>
+        </div>
+        <div class="signup-form mb-2">
+          <v-text-field
+            label="パスワード確認"
+            outlined
+            :append-icon="isHiddenPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="isHiddenPassword ? 'text' : 'password'"
+            @click:append="isHiddenPassword = !isHiddenPassword"
+            v-model="passwordCheck"
+          ></v-text-field>
+        </div>
+        <v-card-actions>
+          <NuxtLink to="#">
+            <v-btn dark width="96" color="#4169e1" @click="signup()">
+              登録
+            </v-btn>
+          </NuxtLink>
+        </v-card-actions>
+      </div>
+    </v-card>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator";
+
+@Component
+export default class Login extends Vue {
+  id = "";
+  email = "";
+  password = "";
+  passwordCheck = "";
+  isHiddenPassword = false;
+
+  signup() {
+    /* ユーザー登録処理 */
+    console.log("signup");
+  }
+}
+</script>
+
+<style scoped>
+.login-card {
+  /* 背景色は決まり次第変更 */
+  background-color: rgba(66, 175, 237, 0.96);
+  height: 100%;
+}
+
+.signup-form {
+  width: 328px;
+}
+</style>
