25287258d8cbf4715aadf5c364c51ecd6143a598
keganokami <400800m@gmail.com>
2021-11-27 02:15:44 +0000

    大会登録フォームの作成（暫定）

--- src/front/pages/auth/dashboard.vue
+++ src/front/pages/auth/dashboard.vue
@@ -2,29 +2,33 @@
 <template>
   <div class="d-flex flex-column pa-4 justify-center align-center">
     <h2>大会作成</h2>
-    <form action="">
+    <v-form ref="form" v-model="valid">
       <div class="">
         <v-text-field
           v-model="compeName"
           name="compe-name"
           label="大会名"
           outlined
+          :rules="inputRules"
         ></v-text-field>
         <v-text-field
           v-model="compePlace"
           name="compe-place"
           label="大会場所"
           outlined
+          :rules="inputRules"
         ></v-text-field>
         <v-text-field
           v-model="compeDates"
           name="compe-dates"
           label="大会日時"
           outlined
+          :rules="inputRules"
         ></v-text-field>
         <v-file-input
           name="compe-guidelines-file"
           chips
+          hidden
           small-chips
           truncate-length="15"
           >大会要項</v-file-input
@@ -39,31 +43,34 @@
           item-key="eventId"
           show-select
           class="elevation-1"
+          :value="selected"
         />
       </div>
       <v-btn
+        :disabled="!valid  || selected.length === 0"
         name="compe-create-btn"
         class="justify-center align-center"
-        dark
         width="170"
         color="#4169e1"
-        @click="onClick()"
+        type="submit"
+        @click="onSubmit()"
       >
         作成
       </v-btn>
-    </form>
+    </v-form>
   </div>
 </template>
  
  
 <script lang="ts">
-import { Component, Vue } from "nuxt-property-decorator";
+import { Component, Vue, Watch } from "nuxt-property-decorator";
 import Compe from "../../domains/compe/Compe";
 import CompeService from "../../domains/compe/CompeService";
 import Events from "../../domains/events/Events";
 
 @Component
 export default class DashBoard extends Vue {
+  valid = true;
   compeName = "";
   compePlace = "";
   compeDates = "";
@@ -72,27 +79,27 @@ export default class DashBoard extends Vue {
     {
       eventId: "001",
       eventName: "男子100m",
-      eventCategory: "SPRINT"
+      eventCategory: "SPRINT",
     },
     {
       eventId: "002",
       eventName: "男子200m",
-      eventCategory: "SPRINT"
+      eventCategory: "SPRINT",
     },
     {
       eventId: "003",
       eventName: "男子300m",
-      eventCategory: "SPRINT"
+      eventCategory: "SPRINT",
     },
     {
       eventId: "004",
       eventName: "男子400m",
-      eventCategory: "SPRINT"
+      eventCategory: "SPRINT",
     },
   ];
 
   singleSelect = false;
-  selected:Events[] = [];
+  selected: Events[] = [];
   headers = [
     {
       text: "id",
@@ -123,14 +130,14 @@ export default class DashBoard extends Vue {
     this.adminId = "admin001";
   }
 
-  onClick() {
-    for(const row of this.selected) {
+  onSubmit() {
+    for (const row of this.selected) {
       const event: Events = {
         eventId: row.eventId,
         eventName: row.eventName,
-        eventCategory: row.eventCategory
-      }
-      this.postEvents.push(event)
+        eventCategory: row.eventCategory,
+      };
+      this.postEvents.push(event);
     }
 
     const compe = new Compe(
@@ -140,11 +147,25 @@ export default class DashBoard extends Vue {
       this.compeDates,
       null,
       this.postEvents
-    )
-debugger
+    );
     this.compeService.createCompe(compe).then(() => {
       console.log("成功");
-    })
+    });
+  }
+
+  // TODO 切り出し
+  inputRules: any = [
+    (v: any) => !!v || "required",
+    (v: any) => this.charRules(v),
+  ];
+
+  private charRules(value: any): any {
+    return value.length <= 50 || "Name must be less than 50 characters";
+  }
+
+  @Watch("selected")
+  isDisableSubmit() {
+    return this.selected.length === 0
   }
 }
 </script>
